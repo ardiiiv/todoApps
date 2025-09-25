cc1c22c6c1cc5ac89bb56703ced2466ad1cf7995
ardiiiv <ardiiiv222@gmail.com>
2025-09-25 20:32:29 +0700

    update html, css dan js

---
+++ js/script.js
@@ -0,0 +1,152 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitForm = document.getElementById("form");
+  submitForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    addTodo();
+  });
+});
+
+function addTodo() {
+  const textTitle = document.getElementById("title").value;
+  const timestamp = document.getElementById("date").value;
+  const description = document.getElementById("description").value;
+
+  const generaredID = generatedId();
+  const todoObject = generatedTodoObject(
+    generaredID,
+    textTitle,
+    timestamp,
+    description,
+    false
+  );
+
+  todos.push(todoObject);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+const todos = [];
+const RENDER_EVENT = "render-todo";
+
+function generatedId() {
+  return +new Date();
+}
+
+function generatedTodoObject(id, task, timestamp, description, isCompleted) {
+  return {
+    id,
+    task,
+    timestamp,
+    description,
+    isCompleted,
+  };
+}
+
+document.addEventListener(RENDER_EVENT, function () {
+  const uncompletedTODOList = document.getElementById("todos");
+  uncompletedTODOList.innerHTML = "";
+
+  const completedTODOList = document.getElementById("completed-todo");
+  completedTODOList.innerHTML = "";
+
+  for (const todoItem of todos) {
+    const todoElement = makeTodo(todoItem);
+    if (!todoItem.isCompleted) {
+      uncompletedTODOList.append(todoElement);
+    } else {
+      completedTODOList.append(todoElement);
+    }
+  }
+});
+
+function makeTodo(todoObject) {
+  const textTitle = document.createElement("h2");
+  textTitle.innerText = todoObject.task;
+
+  const textTimestamp = document.createElement("p");
+  textTimestamp.innerText = todoObject.timestamp;
+
+  const textDescription = document.createElement("p");
+  textDescription.innerText = todoObject.description;
+
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("inner");
+  textContainer.append(textTitle, textTimestamp, textDescription);
+
+  const container = document.createElement("div");
+  container.classList.add("item", "shadow");
+  container.append(textContainer);
+  container.setAttribute("id", `todo-${todoObject.id}`);
+
+  if (todoObject.isCompleted) {
+    const undoButton = document.createElement("button");
+    undoButton.classList.add("undo-button");
+
+    undoButton.addEventListener("click", function () {
+      undoTaskFormCompleted(todoObject.id);
+    });
+
+    const trashButton = document.createElement("button");
+    trashButton.classList.add("trash-button");
+
+    trashButton.addEventListener("click", function () {
+      removeTaskFormCompleted(todoObject.id);
+    });
+
+    container.append(undoButton, trashButton);
+  } else {
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("check-button");
+
+    checkButton.addEventListener("click", function () {
+      addTaskToCompleted(todoObject.id);
+    });
+
+    container.append(checkButton);
+  }
+  return container;
+}
+
+function addTaskToCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget.id == null) return;
+
+  todoTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodo(todoId) {
+  for (const todoItem of todos) {
+    if (todoItem.id === todoId) {
+      return todoItem;
+    }
+  }
+  return null;
+}
+
+function undoTaskFormCompleted(todoId) {
+  const todoTarget = findTodo(todoId);
+
+  if (todoTarget.id == null) return;
+
+  todoTarget.isCompleted = false;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function removeTaskFormCompleted(todoId) {
+  const todoTarget = findTodoIndex(todoId);
+
+  if (todoTarget.id == -1) return;
+
+  todos.splice(todoTarget, 1);
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findTodoIndex(todoId) {
+  for (const index in todos) {
+    if (index.id === todoId) {
+      return index;
+    }
+  }
+  return -1;
+}
